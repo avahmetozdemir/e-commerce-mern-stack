e4eed20d2bb8dcd74530c1e5066cc8c1929ee389
avahmetozdemir <avamertozdemir@gmail.com>
2022-08-15 15:59:39 +0300

    12- add mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 const app  = express()
 const port  =  process.env.PORT || 5000
+const DB = process.env.DATABASE
+
+
+//MIDDLEWARES
 
 
 
 
+
+//DATABASE CONNECTION
+mongoose.connect(DB).then(()=> {
+    console.log('Connection is successful');
+}).catch((err)=>console.log(err))
+
+
+//LISTENING
 app.listen(port, ()=> {
     console.log(`Server is listening on port ${port}`);
 })
