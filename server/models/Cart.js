acfd68c6d03a09618e46e47e997724aec563346e
avahmetozdemir <avamertozdemir@gmail.com>
2022-08-15 17:41:54 +0300

    15 - add cart model

---
+++ server/models/Cart.js
@@ -0,0 +1,26 @@
+import mongoose, { mongo } from "mongoose";
+
+const cartSchema =  new mongoose.Schema({
+    userId : {
+        type: String,
+        required : true,
+    },
+    products : [
+        {
+            productId : {
+                type:String
+            },
+            quantity: {
+                type:Number,
+                default: 1
+            }
+        }
+    ]
+    
+    
+},{timestamps:true})
+
+
+const Cart = mongoose.model('Cart', cartSchema)
+
+export default Cart
