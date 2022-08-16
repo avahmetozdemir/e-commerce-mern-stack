0aa4467de27e6a961836ea85bc6c918717020371
avahmetozdemir <avamertozdemir@gmail.com>
2022-08-16 14:47:11 +0300

    25- add react-router-dom and routes for front-end

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,28 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import SingleProduct from "./pages/SingleProduct";
 import Cart from "./pages/Cart";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
 
 function App() {
+  const user = false
   return (
-    <>
-      {/* <Home/> */}
-    {/* <Login/> */}
-    {/* <Register/> */}
-    {/* <SingleProduct/> */}
-    {/* <Products/> */}
-    <Cart/>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/products/:category" element={<Products />}/>
+      <Route path="/product/:id" element={<SingleProduct />}/>
+      <Route path="/login" element={user ? <Navigate to='/'/> : <Login />}/>
+      <Route path="/register" element={user ? <Navigate to='/'/> : <Register />}/>
+      <Route path="/cart" element={<Cart />}/>
+
+    </Routes>
+  </BrowserRouter>
   );
 }
 
